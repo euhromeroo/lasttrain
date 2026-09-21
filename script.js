c3a76759ad8b060dc14fca95e0015c428e1ab6df
euhromeroo <nicolylinosouza@gmail.com>
2026-09-21 08:26:18 -0300

    yh

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.180.0/build/three.module.js";
-import { PointerLockControls } from "https://cdn.jsdelivr.net/npm/three@0.180.0/examples/jsm/controls/PointerLockControls.js";
+import * as THREE from "three";
+import { PointerLockControls } from "three/addons/controls/PointerLockControls.js";
 
 
 /* =========================================================
